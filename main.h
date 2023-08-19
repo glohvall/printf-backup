e7dbf1039ad52169b5b42020ae62edb8a474ebbc
OizaJ <joettajewel32@gmail.com>
2023-08-19 06:43:02 -0700

    update

--- main.h
+++ main.h
@@ -23,7 +23,8 @@
  *
  * @fmt: The format.
  * @fn: The function associated.
- */
+ *
+ */
 struct fmt
 {
 	char fmt;
@@ -36,7 +37,8 @@ struct fmt
  *
  * @fmt: The format.
  * @fm_t: The function associated.
- */
+ *
+ */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
